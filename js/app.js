ae0f1b8ebb8baae40f53104d860b4af4a58dfc9d
neginfinity85 <neginfinity85@gmail.com>
2023-07-06 15:26:29 +0200

    Added <jQuery functions> to Show&Hide <modal windows>

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(function () {
+  /* filter work category */
   let filter = $('[data-filter]');
 
   filter.on('click', function (event) {
@@ -18,5 +19,49 @@ $(function () {
         }
       });
     }
+  });
+  /* Modal window show&hide */
+
+  const modalCall = $('[data-modal]');
+  const modalClose = $('[data-close]');
+
+  modalCall.on('click', function (event) {
+    event.preventDefault();
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $(modalId).addClass('show');
+    $('body').addClass('no-scroll');
+
+    setTimeout(function () {
+      $(modalId).find('.modal__dialog').css({ transform: 'rotateX(0)' });
+    }, 200);
+  });
+
+  modalClose.on('click', function (event) {
+    event.preventDefault();
+    let $this = $(this);
+    let modalParent = $this.parents('.modal');
+
+    modalParent.find('.modal__dialog').css({ transform: 'rotateX(90deg)' });
+
+    setTimeout(function () {
+      modalParent.removeClass('show');
+      $('body').removeClass('no-scroll');
+    }, 200);
+  });
+
+  $('.modal').on('click', function (event) {
+    let $this = $(this);
+    $this.find('.modal__dialog').css({ transform: 'rotateX(90deg)' });
+
+    setTimeout(function () {
+      $this.removeClass('show');
+      $('body').removeClass('no-scroll');
+    }, 200);
+  });
+
+  $('.modal__dialog').on('click', function (event) {
+    event.stopPropagation();
   });
 });
